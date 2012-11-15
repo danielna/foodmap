dd6207f66783f242c1bf9053bfb5982f9e333808
Daniel Na <daniel.na@gmail.com>
2012-11-15 17:54:02 -0500

    Initial commit, add css/js structure

---
+++ js/foodmap.globals.js
@@ -0,0 +1,9 @@
+
+// Globals
+
+var foodmap = {};
+
+foodmap.globals = {
+    kmlAddress      : "http://www.danielna.com/foodmap/eat.kml",
+    container       : "map-container"
+};
---
+++ js/foodmap.main.js
@@ -0,0 +1,9 @@
+// Run the show
+
+$(document).ready(function() {
+
+   var foodMap = new foodmap.map( foodmap.globals.container, foodmap.globals.kmlAddress );
+
+   foodMap.init();
+
+});
