baddd959d178aa3d938bdf9fb9de1ef9dae779b0
Daniel Na <daniel.na@gmail.com>
2013-02-21 21:22:23 -0500

    remove unused files

--- js/foodmap.globals.js
+++
@@ -1,9 +0,0 @@
-
-// Globals
-
-var foodmap = {};
-
-foodmap.globals = {
-    kmlAddress      : "http://www.danielna.com/foodmap/eat.kml",
-    container       : "map-container"
-};
--- js/foodmap.main.js
+++
@@ -1,9 +0,0 @@
-// Run the show
-
-$(document).ready(function() {
-
-   var foodMap = new foodmap.map( foodmap.globals.container, foodmap.globals.kmlAddress );
-
-   foodMap.init();
-
-});
