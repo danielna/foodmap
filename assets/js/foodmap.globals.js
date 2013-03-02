d5aba2df27cce1b7bf6c1a729da3611e80625878
Daniel Na <daniel.na@gmail.com>
2013-03-01 23:33:53 -0500

    re-add globals.js

---
+++ assets/js/foodmap.globals.js
@@ -0,0 +1,20 @@
+// Globals
+
+var foodmap = {};
+
+foodmap.globals = {
+    API_KEY     : "AIzaSyDiA6472v_0VZUQjmVvXQ4peWbFZsUcpVc",
+    container   : "map-container",
+    map_icons   :   
+                    {
+                        "low"   :   "assets/img/icon_green.png",
+                        "med"   :   "assets/img/icon_yellow.png",
+                        "high"  :   "assets/img/icon_red.png"
+                    },
+    price_map   :   
+                    {
+                        "low"   :   "Cheap Eats",
+                        "med"   :   "Moderately Priced",
+                        "high"  :   "Expensive"
+                    }
+};
